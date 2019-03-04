e615a5b15736c9d322c3d4c9a0925824d5d97eed
Eric Wamsley <eric@ewams.net>
2019-03-04 12:43:44 -0600

    Update names.php

--- names.php
+++ names.php
@@ -1,5 +1,20 @@
 <?php
-
+/*
+##############################LICENSE################################
+#Copyright (c) 2019 Eric Wamsley.                                   #
+#All rights reserved.                                               #
+#                                                                   #
+#Redistribution and use in source and binary forms are permitted    #
+#provided that the above copyright notice and this paragraph are    #
+#duplicated and viewable in all forms and that any documentation,   #
+#advertising materials, and other materials related to such         #
+#distribution and use acknowledge that the software was developed   #
+#by ERIC WAMSLEY. THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT     #
+#ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION,  #
+#THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A        #
+#PARTICULAR PURPOSE.						                        #
+##############################LICENSE################################
+*/
 
 /**
  * names()
